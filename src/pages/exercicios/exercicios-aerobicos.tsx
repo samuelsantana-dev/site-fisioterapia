ae516f249a74b126ac21a79152c71193ca9ae0a5
samuel-santana109 <samuelsantanadasilva8@gmail.com>
2024-05-24 19:16:09 -0300

    refactor: :recycle: testando o sort

--- src/pages/exercicios/exercicios-aerobicos.tsx
+++ src/pages/exercicios/exercicios-aerobicos.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { DivExercicios } from '../../components/div-exercicio';
 import { Exercise } from '../../api/interface';
 import { getExercicios } from '../../api/api-exercicios';
-import { ordenarPorDificuldade } from '../../components/validacoes-gerais';
 
 export const ExerciciosAerobicos = () => {
   const [exercicios, setExercicios] = useState<Exercise[]>([]);
@@ -13,14 +12,13 @@ export const ExerciciosAerobicos = () => {
       .catch((error) => console.error('Erro ao obter exercícios:', error));
   }, []);
 
-  const exerciciosAerobicos = exercicios.filter((ex) => ex.muscle_group === 'aerobicos');
-  ordenarPorDificuldade(exerciciosAerobicos);
+  exercicios.filter((ex) => ex.muscle_group === 'aerobicos').sort((a, b) => a.difficulty - b.difficulty);
 
   return (
     <section className="bg-backgroundMain w-full min-h-screen flex items-center justify-center">
       <div className="w-full">
-        {exerciciosAerobicos.length > 0 && <DivExercicios exercicios={exerciciosAerobicos} />}
-        {exerciciosAerobicos.length === 0 && <span className="loading loading-dots loading-lg"></span> && <p>Não há exercícios aerobicos disponíveis.</p>}
+        {exercicios.length > 0 && <DivExercicios exercicios={exercicios} />}
+        {exercicios.length === 0 && <span className="loading loading-dots loading-lg"></span> && <p>Não há exercícios aerobicos disponíveis.</p>}
       </div>
     </section>
   );
