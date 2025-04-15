99e7e4bb5cea51fdfbf01844868b361d3f0212d1
Samuel Santana <104950258+samuelsantana-dev@users.noreply.github.com>
2025-04-15 19:26:01 -0300

    Update export-padrao.ts
    Nova url

--- src/api/export-padrao.ts
+++ src/api/export-padrao.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "https://app-jadson-back-wvjk3k2iaq-uc.a.run.app";
+export const baseUrl = "https://app-jadson-back-624752169214.us-central1.run.app";
 
 // LISTAR o token
 export const userToken = window.localStorage.getItem("user_token");
