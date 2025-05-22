84b7a518c9c99bbc9e4e8f13567bfc698e226257
Samuel Santana <104950258+samuelsantana-dev@users.noreply.github.com>
2025-05-22 15:12:55 -0300

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,8 +11,8 @@ export function Home(){
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img src={imagemHome} className="max-w-sm rounded-lg shadow-2xl" />
             <div className='max-w-sm'>
-              <h1 className="text-5xl font-bold">Bem-vindo a Fisioterapia!</h1>
-              <p className="py-6">Seja bem-vindo a Fisioterapia, sua plataforma de exercícios de fisioterapia. Aqui você encontrará uma variedade de exercícios projetados para ajudá-lo(a) a melhorar sua saúde e bem-estar. Comece a praticar hoje e sinta a diferença!</p>
+              <h1 className="text-5xl font-bold">Bem-vindo a EXERLAR!</h1>
+              <p className="py-6">Seja bem-vindo a EXERLAR, site educativo com protocolo de exercicios fisicos para pessoas idosas no contexto do lar. Aqui você encontrará uma variedade de exercícios projetados para ajudá-lo(a) a melhorar sua saúde e bem-estar. Comece a praticar hoje e sinta a diferença!</p>
               <Link to="/login" {...buttonPadrao}>Login</Link>
             </div>
           </div>
