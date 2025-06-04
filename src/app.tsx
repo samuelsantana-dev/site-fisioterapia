e3c61564ba59e14859e8ec2bd7648bd4872d1b2d
samuels-space <samuel@00px.com.br>
2025-06-04 16:09:00 -0300

    Ajustado erro de loop no app

--- src/app.tsx
+++ src/app.tsx
@@ -14,14 +14,16 @@ import { ExerciciosAerobicos } from './pages/exercicios/exercicios-aerobicos.tsx
 import { Header } from './components/header.tsx';
 import NotFound from './pages/not-found/not-found.tsx';
 import { Footer } from './components/footer.tsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
     const [isAdmin, setIsAdmin] = useState(true);
     const isUserLoggedIn = window.localStorage.getItem("user_token");
-    if (!isUserLoggedIn) {
-        setIsAdmin(false);
-    }
+    useEffect(() => {
+        if (!isUserLoggedIn) {
+            setIsAdmin(false);
+        }
+    })
     return (
         <div className="App">
             <BrowserRouter>
