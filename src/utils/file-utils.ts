9a41e8bd15b9478a11ba2fbb683d4f4cdc2eedc4
samuels-space <samuel@00px.com.br>
2025-06-10 18:20:03 -0300

    Nova util para exportar em excel

---
+++ src/utils/file-utils.ts
@@ -0,0 +1,11 @@
+export const downloadCSVFile = (content: string, filename: string) => {
+  const blob = new Blob([content], { type: 'text/csv;charset=utf-8;' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+};
